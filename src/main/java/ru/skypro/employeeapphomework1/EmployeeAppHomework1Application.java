9e32e93ea5eec266dd2c60a93652f1773ed8a389
alexdenzero <alexdenzero@yandex.ru>
2023-05-28 18:29:34 +0300

    lists

---
+++ src/main/java/ru/skypro/employeeapphomework1/EmployeeAppHomework1Application.java
@@ -0,0 +1,13 @@
+package ru.skypro.employeeapphomework1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAppHomework1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeAppHomework1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/employeeapphomework1/EmployeeAppHomework1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.employeeapphomework1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAppHomework1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
